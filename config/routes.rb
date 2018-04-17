0f0d941200f04a63ccfc8865b23c44787fa3a41f
Shilpa-Pinnaka <silpa.isha@gmail.com>
2018-04-17 14:11:30 +0000

    We generated dashboards controller so that users-students and instructors can see an overview of the courses they're enrolled in and the courses they are teaching.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
      devise_for :views
      devise_for :users
      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+     resource :dashboard, only: [:show]
+     root 'static_pages#index'
 
-   root 'static_pages#index'
-
-   resources :courses, only: [:index, :show] do
+     resources :courses, only: [:index, :show] do
       resources :enrollments, only: :create
-   end   
-   resources :lessons, only: [:show]
-   namespace :instructor do  
+     end   
+     resources :lessons, only: [:show]
+     namespace :instructor do  
       resources :lessons, only: [:update]
       resources :sections, only: [:update] do
           resources :lessons, only: [:new, :create]
